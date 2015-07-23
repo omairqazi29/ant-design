f3299b175c98fb99f11dad4a4b362fa5c2b66598
afc163 <afc163@gmail.com>
2015-07-23 12:26:13 +0800

    Updates

--- dist/antd-0.7.2.js
+++ dist/antd-0.7.2.js
@@ -23872,73 +23872,48 @@ return /******/ (function(modules) { // webpackBootstrap
 	
 	var defaultDuration = 1.5;
 	
-	var getMessageInstance = function getMessageInstance() {
+	function getMessageInstance() {
 	  return _rcNotification2['default'].newInstance({
 	    prefixCls: 'ant-message',
 	    transitionName: 'move-up',
 	    style: {} // 覆盖原来的样式
 	  });
-	};
+	}
+	
+	function notice(content, _x, type) {
+	  var duration = arguments[1] === undefined ? defaultDuration : arguments[1];
+	
+	  var iconClass = ({
+	    'info': 'anticon-info-circle ant-message-info',
+	    'success': 'anticon-check-circle ant-message-success',
+	    'error': 'anticon-exclamation-circle ant-message-error'
+	  })[type];
+	  getMessageInstance().notice({
+	    key: 'simpleMessage',
+	    duration: duration,
+	    style: {},
+	    content: _react2['default'].createElement(
+	      'div',
+	      { className: 'ant-message-custom-content' },
+	      _react2['default'].createElement('i', { className: 'anticon ' + iconClass }),
+	      _react2['default'].createElement(
+	        'span',
+	        null,
+	        content
+	      )
+	    )
+	  });
+	}
 	
 	exports['default'] = {
-	  info: function info(m, t) {
-	    var message = getMessageInstance();
-	    t = t || defaultDuration;
-	    message.notice({
-	      key: 'simpleMessage',
-	      duration: t,
-	      style: {},
-	      content: _react2['default'].createElement(
-	        'div',
-	        { className: 'ant-message-custom-content' },
-	        _react2['default'].createElement('i', { className: 'anticon anticon-info-circle ant-message-info' }),
-	        _react2['default'].createElement(
-	          'span',
-	          null,
-	          m
-	        )
-	      )
-	    });
+	  info: function info(content, duration) {
+	    notice(content, duration, 'info');
 	  },
-	  success: function success(m, t) {
-	    var message = getMessageInstance();
-	    t = t || defaultDuration;
-	    message.notice({
-	      key: 'simpleMessage1',
-	      transitionName: 'move-up',
-	      duration: t,
-	      style: {},
-	      content: _react2['default'].createElement(
-	        'div',
-	        { className: 'ant-message-custom-content' },
-	        _react2['default'].createElement('i', { className: 'anticon anticon-check-circle ant-message-success' }),
-	        _react2['default'].createElement(
-	          'span',
-	          null,
-	          m
-	        )
-	      )
-	    });
+	  success: function success(content, duration) {
+	    notice(content, duration, 'success');
 	  },
-	  error: function error(m, t) {
-	    var message = getMessageInstance();
-	    t = t || defaultDuration;
-	    message.notice({
-	      key: 'simpleMessage2',
-	      transitionName: 'move-up',
-	      duration: t,
-	      style: {},
-	      content: _react2['default'].createElement(
-	        'div',
-	        { className: 'ant-message-custom-content' },
-	        _react2['default'].createElement('i', { className: 'anticon anticon-exclamation-circle ant-message-error' }),
-	        _react2['default'].createElement(
-	          'span',
-	          null,
-	          m
-	        )
-	      )
-	    });
+	  error: function error(content, duration) {
+	    notice(content, duration, 'error');
 	  }
 	};
 	module.exports = exports['default'];
