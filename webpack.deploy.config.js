d9e1ba12824c6e381b7f8d040d7c172ad2eb0dca
afc163 <afc163@gmail.com>
2016-01-18 10:57:45 +0800

    Updates

--- webpack.deploy.config.js
+++ webpack.deploy.config.js
@@ -1,12 +1,4 @@
 var webpack = require('webpack');
 var config = require('./webpack.config');
 
-delete config.devtool;
-config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-  sourceMap: false,
-  output: {
-    ascii_only: true
-  }
-}));
-
 module.exports = config;
