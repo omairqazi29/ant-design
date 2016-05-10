820207d185d7e36b6cf1ed51326898c7f39f3380
afc163 <afc163@gmail.com>
2016-05-11 00:44:52 +0800

    Updates

--- _data/practice.js
+++
@@ -1,4 +0,0 @@
-module.exports = {
-  'docs/practice/bussiness.md': require('../docs/practice/bussiness.md'),
-  'docs/practice/cases.md': require('../docs/practice/cases.md'),
-};
--- _data/resource.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  'docs/resource/download.md': require('../docs/resource/download.md'),
-  'docs/resource/github.md': require('../docs/resource/github.md'),
-  'docs/resource/reference.md': require('../docs/resource/reference.md'),
-};
