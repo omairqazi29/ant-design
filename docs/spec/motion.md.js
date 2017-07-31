63aff28cd61496832986e2f422e39f1701d89d81
Wei Zhu <yesmeck@gmail.com>
2017-07-31 11:36:20 +0800

    Updates

--- docs/spec/motion.md.js
+++ docs/spec/motion.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([61,218],{1297:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
+webpackJsonp([61,219],{1162:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
