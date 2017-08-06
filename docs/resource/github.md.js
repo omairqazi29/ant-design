1579f189896fb113ff96050b7503968f63c4b310
乐仪 <linya.qu@alipay.com>
2017-08-06 21:10:50 +0800

    Updates

--- docs/resource/github.md.js
+++ docs/resource/github.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([91,219],{1269:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
+webpackJsonp([91,220],{1269:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
