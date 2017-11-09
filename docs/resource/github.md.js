49a81bb4faa5b10591a6621923eac00dcc5e02c2
afc163 <afc163@gmail.com>
2017-11-09 16:56:50 +0800

    Updates

--- docs/resource/github.md.js
+++ docs/resource/github.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([91,220],{1290:function(t,e){t.exports={content:["article"],meta:{order:2,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
+webpackJsonp([92,221],{1290:function(t,e){t.exports={content:["article"],meta:{order:3,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
