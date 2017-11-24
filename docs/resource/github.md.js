3fd0f6dcc2ec8175f0b61f6c888737865a96aeeb
Wei Zhu <yesmeck@gmail.com>
2017-11-24 15:38:09 +0800

    Updates

--- docs/resource/github.md.js
+++ docs/resource/github.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([92,221],{1289:function(t,e){t.exports={content:["article"],meta:{order:3,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
+webpackJsonp([92,221],{1340:function(t,e){t.exports={content:["article"],meta:{order:3,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
