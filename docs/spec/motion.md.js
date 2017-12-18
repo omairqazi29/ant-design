a1d8c0c93c3ec6c5d41efb894f07840d210b39db
afc163 <afc163@gmail.com>
2017-12-18 11:08:09 +0800

    Updates

--- docs/spec/motion.md.js
+++ docs/spec/motion.md.js
@@ -1,1 +1,1 @@
-webpackJsonp([63],{1765:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
+webpackJsonp([63],{1766:function(t,n){t.exports={content:["article"],meta:{order:12,title:{"zh-CN":"\u52a8\u6548","en-US":"Motion"},link:"http://motion.ant.design",filename:"docs/spec/motion.md"},toc:["ul"]}}});
