db6b93ee7793d69640e8586fb7ecc6d1b7081517
Wei Zhu <yesmeck@gmail.com>
2018-01-06 09:42:10 +0800

    Updates

--- docs/resource/github.md.js
+++
@@ -1,1 +0,0 @@
-webpackJsonp([94],{1734:function(t,e){t.exports={content:["article"],meta:{order:3,link:"//github.com/ant-design/ant-design",title:"GitHub",filename:"docs/resource/github.md"},toc:["ul"]}}});
